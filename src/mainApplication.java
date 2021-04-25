53f5e590bd3f5b07a9dbae902165df8e8a39f3f7
Zugidor <zugidor@tutanota.com>
2021-04-25 17:20:14 +0100

    cleaned up CLI code and optimised query 3

--- src/mainApplication.java
+++ src/mainApplication.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class mainApplication {
+public class mainApplication
+{
 
     public static final String firstTitle =
     		"          _   _                                             ______           \r\n" +
@@ -25,84 +26,129 @@ public class mainApplication {
     		"	                                  |___/                               " +
     		"	                                                              \r\n";
     
-    public static final String queryTable = "+-------+----------------------------------------------------------------------------+\n" +
-                                            "| Query |                                  Action                                    |\n" +
-                                            "+-------+----------------------------------------------------------------------------+\n" +
-                                            "|   1   | Receive a list of stops between 2 bus stops alongside the associated cost. |\n" +
-                                            "+-------+----------------------------------------------------------------------------+\n" +
-                                            "|   2   | Search for a bus stop by it's full name or by the first few characters.    |\n" +
-                                            "+-------+----------------------------------------------------------------------------+\n" +
-                                            "|   3   | Search for all trips with a given arrival time sorted by Trip ID.          |\n" +
-                                            "+-------+----------------------------------------------------------------------------+\n" +
-                                            "|   4   | Exit the program.                                                          |\n" +
-                                            "+-------+----------------------------------------------------------------------------+";
-    public static void main(String[] args) throws IOException {
-        //Print out the title with the title text and a banner.
+    public static final String queryTable =
+            "+-------+----------------------------------------------------------------------------+\n" +
+            "| Query |                                  Action                                    |\n" +
+            "+-------+----------------------------------------------------------------------------+\n" +
+            "|   1   | Receive a list of stops between 2 bus stops alongside the associated cost. |\n" +
+            "+-------+----------------------------------------------------------------------------+\n" +
+            "|   2   | Search for a bus stop by it's full name or by the first few characters.    |\n" +
+            "+-------+----------------------------------------------------------------------------+\n" +
+            "|   3   | Search for all trips with a given arrival time sorted by Trip ID.          |\n" +
+            "+-------+----------------------------------------------------------------------------+\n" +
+            "|   4   | Exit the program.                                                          |\n" +
+            "+-------+----------------------------------------------------------------------------+";
+    public static void main(String[] args) throws IOException
+    {
+        // Print out the title with the title text and a banner.
         System.out.println(String.join("", Collections.nCopies(86,"*")));
         System.out.println(firstTitle);
         System.out.println(secondTitle);
         System.out.println(String.join("", Collections.nCopies(86,"*")));
-        System.out.println(String.join("", Collections.nCopies(13, " ")) + "This system provides 4 unique user queries at your disposal.");
-        System.out.println(String.join("", Collections.nCopies(12, " ")) + "Simply enter any query number from the table of queries below.");
-        System.out.println(queryTable);
+        System.out.println(String.join("", Collections.nCopies(13, " "))
+                + "This system provides 4 unique user queries at your disposal.");
+        System.out.println(String.join("", Collections.nCopies(12, " "))
+                + "Simply enter any query number from the table of queries below.");
+        
         Scanner scanner = new Scanner(System.in);
         boolean runApp = true;
-        boolean validQuery = false;
-        while(runApp){
+        boolean query3RunPrev = false;
+        Map<String, List<stopTime>> stopTimes = null;
+        
+        while(runApp)
+        {
+            System.out.println(queryTable);
             System.out.print("\nEnter your query: ");
             String userInput = scanner.next();
-            validQuery = isValidQuery(userInput);
-            int requestedQuery = 0;
-            if(validQuery){
-                requestedQuery = Integer.parseInt(userInput);
-            }
-            if(requestedQuery == 1){
-                System.out.println("Sorry, this feature is still being developed.");
-            }
-            else if(requestedQuery == 2){
-                System.out.println("Sorry, this feature is still being developed.");
-            }
-            else if(requestedQuery == 3){
-                boolean runUserQuery3 = true;
-                Map<String, List<stopTime>> stopTimes = stopTime.generateHashMapOfStopTimes("input/stop_times.txt");
-                while(runUserQuery3) {
-                    System.out.print("Input an arrival time in the format 'hh:mm:ss': ");
-                    String userArrivalTimeInput = scanner.next();
-                    stopTime.findListOfTripsWithGivenArrivalTime(userArrivalTimeInput, stopTimes);
-                    boolean innerQuery = true;
-                    while (innerQuery) {
-                        System.out.print("Do you want to search for another arrival time? [Y/N]: ");
-                        String userReply = scanner.next();
-                        if (userReply.equals("N") || userReply.equals("n")) {
-                            innerQuery = false;
-                            runUserQuery3 = false;
+            
+            switch (userInput)
+            {
+                case "1":
+                    System.out.println("Sorry, this feature is still being developed.\n");
+                    break;
+                case "2":
+                    System.out.println("Sorry, this feature is still being developed.\n");
+                    break;
+                case "3":
+                    boolean runUserQuery3 = true;
+                    if (!query3RunPrev)
+                    {
+                        stopTimes = stopTime.generateHashMapOfStopTimes("input/stop_times.txt");
+                        query3RunPrev = true;
+                    }
+                    while (runUserQuery3)
+                    {
+                        System.out.print("Input an arrival time in the format 'hh:mm:ss': ");
+                        String userArrivalTimeInput = scanner.next();
+                        userArrivalTimeInput = userArrivalTimeInput.trim();
+                        if (validTimeFormat(userArrivalTimeInput))
+                        {
+                            stopTime.findListOfTripsWithGivenArrivalTime(userArrivalTimeInput, stopTimes);
+                            boolean innerQuery = true;
+                            while (innerQuery)
+                            {
+                                System.out.print("Do you want to search for another arrival time? [Y/N]: ");
+                                String userReply = scanner.next();
+                                if (userReply.equalsIgnoreCase("N"))
+                                {
+                                    innerQuery = false;
+                                    runUserQuery3 = false;
+                                } else if (userReply.equalsIgnoreCase("Y"))
+                                {
+                                    innerQuery = false;
+                                } else
+                                {
+                                    System.out.println("Please provide a valid answer.");
+                                }
+                            }
                         }
-                        else if (userReply.equals("Y") || userReply.equals("y")){
-                            innerQuery = false;
-                        }
-                        else {
-                            System.out.println("Please provide a valid answer.");
+                        else
+                        {
+                            System.out.println("Please enter a valid time.");
                         }
                     }
-                }
-            }
-            else if(requestedQuery == 4){
-                System.out.println("Thank you for using the Vancouver Bus Management System.");
-                runApp = false;
-            }
-            else{
-                System.out.println("Please enter a valid query number.");
+                    break;
+                case "4":
+                    System.out.println("\nThank you for using the Vancouver Bus Management System.");
+                    runApp = false;
+                    break;
+                default:
+                    System.out.println("Please enter a valid query number.\n");
+                    break;
             }
         }
         scanner.close();
     }
-
-    public static boolean isValidQuery(String s) {
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch (NumberFormatException nfe) {
+    
+    static private boolean validTimeFormat(String s)
+    {
+        if (s == null || s.length() != 8)
+        {
             return false;
         }
+        if (s.charAt(2) == ':' && s.charAt(5) == ':')
+        {
+            String hh = s.substring(0, 2);
+            String mm = s.substring(3, 5);
+            String ss = s.substring(6, 8);
+            int hours = -1, minutes = -1, seconds = -1;
+            try
+            {
+                hours = Integer.parseInt(hh);
+                minutes = Integer.parseInt(mm);
+                seconds = Integer.parseInt(ss);
+                if (hours > -1 && hours < 24 &&
+                    minutes > -1 && minutes < 60 &&
+                    seconds > -1 && seconds < 60)
+                {
+                    return true;
+                }
+            }
+            catch (NumberFormatException nfe)
+            {
+                return false;
+            }
+        }
+        return false;
     }
 }
