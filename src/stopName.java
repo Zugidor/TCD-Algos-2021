3b7ab202eddae1dd468fdc5f1dbf926a79cb932e
Zugidor <zugidor@tutanota.com>
2021-04-27 16:43:33 +0100

    removed unnecessary import

--- src/stopName.java
+++ src/stopName.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
